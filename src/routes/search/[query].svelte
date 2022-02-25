6fb84b632a6fbd00f58291af15f3e53ebbf17c94
lem6ns <lemons@riseup.net>
2022-02-25 13:57:01 -0700

    Work on watch page, work on gomo and setup debounce
    Work on video page and gomostream.

---
+++ src/routes/search/[query].svelte
@@ -0,0 +1,11 @@
+<script type="ts">
+    let timer;
+    let results = [];
+    
+    const debounce = (e: string) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            console.log(e);
+        }, 1000);
+    };
+</script>
---
+++ src/routes/watch/[quality]/[type]/[provider]/[slug].svelte
@@ -0,0 +1,51 @@
+<script>
+    import { page } from "$app/stores";
+    const { provider, slug, type, quality } = $page.params;
+    let url = "";
+    let error = "";
+
+    const fetchData = async () => {
+        const json = await fetch(
+            `/api/get/scrapers/${provider}/${type}/${encodeURIComponent(slug)}`
+        ).then((r) => r.json());
+
+        if (json.qualities.length && quality != "default") {
+            for (const q of json.qualities) {
+                if (q.quality === quality) {
+                    url = q.url;
+                    break;
+                }
+            }
+        } else {
+            url = json.url;
+        };
+
+        if (!url) error = "Invalid quality.";
+
+        return json;
+    };
+</script>
+
+{#await fetchData()}
+    <h1>Loading...</h1>
+{:then info}
+    {#if error}
+        <h1>{ error }</h1>
+    {:else}
+        <!-- svelte-ignore a11y-media-has-caption -->
+        <video controls>
+            <source src={url} type="video/mp4" />
+        </video>
+
+        {#if info.qualities}
+            <h2>Qualities</h2>
+            <ul>
+                {#each info.qualities as { quality }}
+                    <li>{quality}</li>
+                {/each}
+            </ul>
+        {/if}
+
+        <h2>Current Quality: { quality }</h2>
+    {/if}
+{/await}
