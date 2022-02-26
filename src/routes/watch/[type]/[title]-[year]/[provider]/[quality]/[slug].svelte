4f80157c3a0d67e3b3218792391a248d73be6b1f
lem6ns <lemons@riseup.net>
2022-02-25 22:42:40 -0700

    Workaround for vembed & gogo

--- src/routes/watch/[type]/[title]-[year]/[provider]/[quality]/[slug].svelte
+++ src/routes/watch/[type]/[title]-[year]/[provider]/[quality]/[slug].svelte
@@ -31,7 +31,7 @@
         if (isNaN(parseInt(year))) {
             title = `${title}-${year.slice(0, -5)}`;
             year = year.slice(-4);
-        };
+        }
 
         return json;
     };
@@ -40,7 +40,24 @@
 {#await fetchData()}
     <h1>Loading...</h1>
 {:then info}
-    {#if error}
+    {#if provider == "gogo" || "vembed"}
+        <code>{url}</code>
+        <h1>Use a video player like <a href="https://wiki.videolan.org/VLC_command-line_help/">VLC</a> or <a href="https://mpv.io/manual/master/#options-referrer">MPV</a> to play this video above. <strong>PLEASE</strong> set the referrer to <code>{provider == "gogo" ? "https://gogoplay.io" : "https://vidembed.io"}</code>, or it will not work.</h1>
+        <br>
+        {#if info.qualities}
+            <h2>Qualities</h2>
+            <ul>
+                {#each info.qualities as { quality }}
+                    <li>{quality}</li>
+                {/each}
+            </ul>
+        {/if}
+
+        <h2>Title: {title}</h2>
+        <h2>Year: {year}</h2>
+
+        <h2>Current Quality: {quality}</h2>
+    {:else if error}
         <h1>{error}</h1>
     {:else}
         <!-- svelte-ignore a11y-media-has-caption -->
